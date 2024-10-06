47ba2ebc5943cbe3bbbd16c8ddc1425a84dc28be
AgustinPatun <agusplatun@hotmail.com>
2024-10-06 16:53:12 -0300

    se separo bien en carpetas las clases

--- SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
+++ SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3e5932edbb1ebe3199aa1428e7514bdade9c78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1897ec43842f7b231cc9b033e5139a675a939214")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
