1dc9dc6f2b3add1e2fcae1c08024030bc6d71c87
AgustinPatun <agusplatun@hotmail.com>
2024-10-07 17:13:44 -0300

    asd

--- SGI/SGI.Consola/obj/Debug/net8.0/SGI.Consola.AssemblyInfo.cs
+++ SGI/SGI.Consola/obj/Debug/net8.0/SGI.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGI.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2519d66e826bd177f3a2de8932bc0ae2bcebc9bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684a4f269431c6006b2deadbb31c60d42155e69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGI.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGI.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
