925db128e5ebf6e848c8ba1cb36b713937b43a30
almadasantiago <santiagoalmada111@hotmail.com>
2024-10-07 15:41:47 -0300

    actualizamos rama

---
+++ SGI/SGI.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+using SGI.Repositorios;
+using System.IO;
+
+Menu menu = new Menu();
+while(!menu.opcion.Equals("0"))
+{
+    menu.invocarMenu();
+    menu.opcion = Console.ReadLine() ?? "999";
+    menu.elegirOpciones();
+}
+Console.ReadKey();
