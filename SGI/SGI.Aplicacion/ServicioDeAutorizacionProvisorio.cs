0aae25a34bc417f08b0ba49e93cae181cfc9b653
FrancoBanis <franco_banis0@gmail.com>
2024-10-09 00:09:27 -0300

    Se agrego el ServicioDeAutorizacion.

---
+++ SGI/SGI.Aplicacion/ServicioDeAutorizacionProvisorio.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+public class ServicioDeAutorizacionProvisorio : IServicioDeAutorizacion{
+    public bool PoseeElPermiso (int idUsuario, List<bool> permisos)
+    {
+        if (idUsuario == 1) 
+        {
+            permisos.
+        }
+        return false;
+    }
+}
