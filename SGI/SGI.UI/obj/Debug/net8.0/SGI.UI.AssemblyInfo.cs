2f989dfd918dc1b08fa4f975aff3ba3676ea5f3c
AgustinPatun <agusplatun@hotmail.com>
2024-11-17 14:32:11 -0300

    se crearon las pestañas nuevas

--- SGI/SGI.UI/obj/Debug/net8.0/SGI.UI.AssemblyInfo.cs
+++ SGI/SGI.UI/obj/Debug/net8.0/SGI.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGI.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b9a692030bbfa4736bd627d05372ded9bc7e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de35d27c315241c4baa66f10da0fb7c1b3b6b7f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGI.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGI.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
