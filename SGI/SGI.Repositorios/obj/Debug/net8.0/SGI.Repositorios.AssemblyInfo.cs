028d679560215e42c0745cb259204b0ad3bc866e
AgustinPatun <agusplatun@hotmail.com>
2024-11-27 21:34:29 -0300

    se movio el gitignore al lugar correcto

--- SGI/SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
+++ SGI/SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd23091f992e5823d19a1ab0e5c2f699b14a1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eee0ad63eb27f5762ff14caa2a83e2927eeb8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
