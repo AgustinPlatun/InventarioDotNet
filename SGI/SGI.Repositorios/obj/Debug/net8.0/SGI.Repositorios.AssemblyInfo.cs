aacbe09c9be3a053fc97b016c202e58e551bf5e3
AgustinPatun <agusplatun@hotmail.com>
2024-12-02 17:53:33 -0300

    gitignore

--- SGI/SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
+++ SGI/SGI.Repositorios/obj/Debug/net8.0/SGI.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1c672bb36ec02cf572a6543ab43af961eb951c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e619cd26abdc5b1db931963d3c84f734dad905")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGI.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
