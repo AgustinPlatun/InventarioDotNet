2e59d118269677e72d377d3b96e757ca4f3c956b
FrancoBanis <franco_banis0@gmail.com>
2024-12-03 17:58:36 -0300

    Se agrego que las transacciones asociadas a un producto se eliminen cuando este es dado de baja.

--- SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarBajaProducto.cs
+++ SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarBajaProducto.cs
@@ -3,11 +3,6 @@ namespace SGI.Aplicacion;
 using SGI.Aplicacion;
 public class DarBajaProducto(IProductoRepositorio repositorio, ServicioAutorizacion autorizacion) {
     public void Ejecutar (int idUsuario,int idProducto) { 
-        Permiso.Permisos permiso = Permiso.Permisos.productobaja;
-        if(autorizacion.PoseeElPermiso(permiso,idUsuario)){
             repositorio.ProductoBaja(idProducto);
-        }else{
-            throw new AutorizacionException("No posee el permiso necesario para realizar una baja de producto !");
-        }
     }
 }
