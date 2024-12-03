51c5e4ac00ca0a0ede4ae5089412c1c48f6350b4
AgustinPatun <agusplatun@hotmail.com>
2024-12-02 22:41:35 -0300

    se agregaron manejo de excepciones para los permisos de la alta y de la baja

--- SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarAltaProducto.cs
+++ SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarAltaProducto.cs
@@ -1,16 +1,16 @@
 using System.Security;
 namespace SGI.Aplicacion;
 using SGI.Aplicacion;
-public class DarAltaProducto(IProductoRepositorio repo, ServicioAutorizacion autorizacion) {
+public class DarAltaProducto(IProductoRepositorio repositorio, ServicioAutorizacion autorizacion) {
     public void Ejecutar (int idUsuario, string nombre , string descripcion, double precioUnitario, int stockDisponible,int idCategoria) { 
         Permiso.Permisos permiso = Permiso.Permisos.productoalta;
         if(autorizacion.PoseeElPermiso(permiso,idUsuario)){
             ProductoValidador validador = new ProductoValidador();
             validador.ValidarPrecioUnitario(precioUnitario);
             validador.ValidarStockDisponible(stockDisponible);
-            repo.ProductoAlta(idUsuario,nombre,descripcion,precioUnitario,stockDisponible,idCategoria);
+            repositorio.ProductoAlta(nombre,descripcion,precioUnitario,stockDisponible,idCategoria);
         }else{
-            throw new AutorizacionException("No posee el permiso necesario para crear un tramite");
+            throw new AutorizacionException("No posee el permiso necesario para realizar una alta de producto !");
         }
     }
 
--- SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarBajaProducto.cs
+++ SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoProducto/DarBajaProducto.cs
@@ -1,8 +1,13 @@
 using System.Security;
 namespace SGI.Aplicacion;
 using SGI.Aplicacion;
-public class DarBajaProducto(IProductoRepositorio repo) {
-    public void Ejecutar (int idProducto) { 
-            repo.ProductoBaja(idProducto);
+public class DarBajaProducto(IProductoRepositorio repositorio, ServicioAutorizacion autorizacion) {
+    public void Ejecutar (int idUsuario,int idProducto) { 
+        Permiso.Permisos permiso = Permiso.Permisos.productobaja;
+        if(autorizacion.PoseeElPermiso(permiso,idUsuario)){
+            repositorio.ProductoBaja(idProducto);
+        }else{
+            throw new AutorizacionException("No posee el permiso necesario para realizar una baja de producto !");
+        }
     }
 }
