7fdfd393c5d42fe0aa0e23f9fd40221de00200b3
AgustinPatun <agusplatun@hotmail.com>
2024-12-04 02:28:45 -0300

    usuario modificacion creado

--- SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoUsuario/CasoDeUsoUsuarioModificar.cs
+++ SGI/SGI.Aplicacion/CasosDeUso/CasosDeUsoUsuario/CasoDeUsoUsuarioModificar.cs
@@ -3,6 +3,7 @@ using SGI.Aplicacion;
 namespace SGI.Aplicacion;
 public class CasoDeUsoUsuarioModificar (IUsuarioRepositorio repo){
     public void Ejecutar(int? idUsuario,string nombre, string apellido, string email, string password){
-        repo.UsuarioModificacion(idUsuario,nombre,apellido,email, password); 
+        string passwordPasadaPorHash = ServicioFuncionHash.FuncionHashSHA256(password);
+        repo.UsuarioModificacion(idUsuario,nombre,apellido,email, passwordPasadaPorHash); 
     }
 }
